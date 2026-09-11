cb4a2c09e1d27e59847d18272e63e0aeb9af7094
Sanket-378 <sanketkhatkale07@gmail.com>
2026-09-11 23:38:00 +0530

    Add gitignore

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,7 @@
-const API_BASE_URL = (window.__COLLEGE_API_BASE_URL__ || 'http://localhost:8081').replace(/\/$/, '');
-
+const API_BASE_URL = (
+  window.__COLLEGE_API_BASE_URL__ ||
+  'https://college-finder-api-amw0.onrender.com'
+).replace(/\/$/, '');
 export async function apiRequest(path, options = {}) {
   const response = await fetch(`${API_BASE_URL}${path}`, {
     ...options,
